6492641a16c69479e1987e687c2b79f124a1943d
Angelo Pablo <angelopablo014@gmail.com>
2020-06-02 20:46:17 -0600

    added week 7 stuff

---
+++ week7/hikes5/comment.js
@@ -0,0 +1,21 @@
+export class CommentList {
+    comments = [];
+    parent = document.getElementById('hikes');
+
+    constructor() {
+        //add eventlistener
+        
+    }
+    getAllComments() {
+
+    }
+    renderCommentList() {
+
+    }
+    filterCommentsById() {
+
+    }
+    addComments() {
+
+    }
+}
