89d0aefc5385642d615f3fdedcfa0c4b47843a3d
Angelo Pablo <angelopablo014@gmail.com>
2020-06-09 10:05:23 -0600

    worked more on the final project

--- finalProject/scripts/main.js
+++ finalProject/scripts/main.js
@@ -1,20 +1,62 @@
-proxyurl = "https://cors-anywhere.herokuapp.com/"
-api = "RGAPI-83857343-3a08-44bf-b12e-6eb687fb3214"
-
+import { Summoner } from "./summoner.js";
+let api = "RGAPI-83857343-3a08-44bf-b12e-6eb687fb3214";
+let users = [];
 window.addEventListener('load', function() {
-    const url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/N3WBKILR?api_key=" + api;
+    getUsers();
+})
+document.getElementById('add').addEventListener('click', addUsers);
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+function addUsers() {
+    let name = document.getElementById("name").value;
+    let newSummoner;
+    let response = "this didnt work"
+    const url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+ name +"?api_key=" + api;
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200) {
-            let response = JSON.parse(this.responseText)
-            console.log(this.responseText)
-            getSummonerId(response)
+            response = JSON.parse(this.responseText)
+            newSummoner = new Summoner(response.id, response.name)
+            //alert (newSummoner.name)
+            //sleep(3000)
+            //users.push(newSummoner)
+            // localStorage.setItem('users', users)
         }
     }
-    xhttp.open("GET", url, true);
+    xhttp.open("GET", url, true)
+    xhttp.onload = function() {
+        // response = JSON.parse(this.responseText)
+        // newSummoner = new Summoner(response.id, response.name)
+        alert (newSummoner.name)
+        users.push(newSummoner)
+    }
     xhttp.send()
-})
-
-function getSummonerId(response) {
-    document.querySelector("main").innerHTML = response.name;
 }
+
+function getUsers() {
+    if(users = localStorage.getItem("users")) {
+        document.getElementById("users").innerHTML = users[0].name
+    }
+    else {
+        document.getElementById("users").innerHTML = "You currently have no players on your team"
+    }
+}
+
+// function getSummonerInfo(summoner) {
+//     let newSummoner;
+//     const url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+ summoner +"?api_key=" + api;
+//     var xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function() {
+//         if(this.readyState == 4 && this.status == 200) {
+//             let response = JSON.parse(this.responseText)
+//             console.log(response.id)
+//             newSummoner = new Summoner(response.id, response.name)
+//             return newSummoner;
+//         }
+//     }
+//     xhttp.open("GET", url, true);
+//     xhttp.send()
+// }
---
+++ finalProject/scripts/summoner.js
@@ -0,0 +1,7 @@
+export class Summoner {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        //this.rank = rank;
+    }
+}
