a5097cc44b404bddfff100721c79d49ea55c3779
Angelo Pablo <angelopablo014@gmail.com>
2020-06-16 16:03:00 -0600

    added week 9 team activity

---
+++ week9/javascript30-drums/teamActivity.js
@@ -0,0 +1,62 @@
+
+
+window.addEventListener('keypress', (e) => {
+    if (event.defaultPrevented) {
+        console.log('error')
+        return; // Do nothing if the event was already processed
+      }
+
+      let audio
+      switch(e.key) {
+        case 'a': 
+            audio = new Audio('sounds/clap.wav')
+            audio.play()
+            console.log(document.getElementById(e.key))
+            break
+        case 's': 
+            audio = new Audio('sounds/hihat.wav')
+            audio.play()
+            break
+        case 'd': 
+            audio = new Audio('sounds/kick.wav')
+            audio.play()
+            break
+        case 'f':
+            audio = new Audio('sounds/openhat.wav')
+            audio.play()
+            break
+        case 'g':
+            audio = new Audio('sounds/boom.wav')
+            audio.play()
+            break
+        case 'h':
+            audio = new Audio('sounds/ride.wav') 
+            audio.play()               
+
+            break;
+        case 'j':
+            audio = new Audio('sounds/snare.wav')
+            audio.play()
+            break;
+        case 'k':
+            audio = new Audio('sounds/tom.wav')
+            audio.play()
+            break
+        case 'l':
+            audio = new Audio('sounds/tink.wav')
+            audio.play()
+            break
+      }
+
+      const button = document.getElementById(e.key)
+      const margin = parseInt(button.style.marginTop.replace('px', ''))
+      button.style.marginTop = margin + 100 + 'px'
+      //ßdocument.getElementById(e.key).style.marginTop = '10px'
+      audio.onplaying = (event) => {
+          console.log('in playing')
+          document.getElementById(e.key).classList.add('playing')
+      }
+     audio.onended = (event) => {
+         document.getElementById(e.key).classList.remove('playing')
+       }
+})
