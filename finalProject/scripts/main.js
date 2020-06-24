e99b2a91020254aedd7c922eb5f94bb19ebbd734
Angelo Pablo <angelopablo014@gmail.com>
2020-06-23 18:26:30 -0600

    literally spent two hours and got absolutely nowhere

--- finalProject/scripts/main.js
+++ finalProject/scripts/main.js
@@ -1,34 +1,65 @@
 import { Summoner } from "./summoner.js";
-let api = "RGAPI-842298e3-51df-49b7-819f-6e6cf18e0579";
+
+let api = "RGAPI-defef8b1-768e-4e79-9efa-e89ced0056a9";
 let users = [];
 window.addEventListener('load', function() {
     getUsers();
 })
+
 document.getElementById('add').addEventListener('click', addUsers);
 
-// function sleep(ms) {
-//     return new Promise(resolve => setTimeout(resolve, ms));
-//   }
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
+  }
 
-function addUsers() {
-    let name = document.getElementById("name").value;
+/*
+This Function is used to grab a single summoner information using the api call. In order to keep the data
+we store the summoner data in local storage
+*/
+function getSingleSummoner(name) {
     let newSummoner;
     const url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+ name +"?api_key=" + api;
     
     fetch(url)
     .then(response => response.json())
     .then(summonerInfo => {
-        console.log(summonerInfo)
+        //console.log(summonerInfo)
         newSummoner = new Summoner(summonerInfo['id'], summonerInfo['name']);
-        // users.push[newSummoner]
         localStorage.setItem('userInfo', JSON.stringify(newSummoner))
     })
+}
 
-    let info = JSON.parse(localStorage.getItem('userInfo'))
-    newSummoner = new Summoner(info.id, info.name)
-    console.log(newSummoner.name)
-    //users.push(newSummoner)
-    //localStorage.setItem('users', users)
+/*
+This function will allow users to add summoners on their team and it is activated by placing the name of
+a summoner onto the textbox this will grab usere info store it into local storage and store into an array
+of summoners
+*/
+function addUsers() {
+    // let name = document.getElementById("name").value;
+    // const url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+ name +"?api_key=" + api;
+    // getSingleSummoner(name)
+
+    // setTimeout(function() {
+    //     users.push(newSummoner)
+    // }, 2000)
+    // let newSummoner = new Summoner(info.id, info.name)
+    let newSummoner;
+    let name = document.getElementById('name').value
+    const url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + "?api_key=" + api;
+    
+    fetch(url)
+    .then(response => response.json())
+    .then(summonerInfo => {
+        //console.log(summonerInfo)
+        newSummoner = new Summoner(summonerInfo.id, summonerInfo.name);
+        localStorage.setItem('userInfo', JSON.stringify(newSummoner))
+        users.push[newSummoner]
+    })
 }
 
 function getUsers() {
@@ -39,19 +70,3 @@ function getUsers() {
         document.getElementById("users").innerHTML = "You currently have no players on your team"
     }
 }
-
-// function getSummonerInfo(summoner) {
-//     let newSummoner;
-//     const url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+ summoner +"?api_key=" + api;
-//     var xhttp = new XMLHttpRequest();
-//     xhttp.onreadystatechange = function() {
-//         if(this.readyState == 4 && this.status == 200) {
-//             let response = JSON.parse(this.responseText)
-//             console.log(response.id)
-//             newSummoner = new Summoner(response.id, response.name)
-//             return newSummoner;
-//         }
-//     }
-//     xhttp.open("GET", url, true);
-//     xhttp.send()
-// }
