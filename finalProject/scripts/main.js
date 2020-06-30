e8e17478eaa4baaa7a104c5aff9fe078f2b32d57
Angelo Pablo <angelopablo014@gmail.com>
2020-06-30 14:55:23 -0600

    added json stuff

--- finalProject/scripts/main.js
+++ finalProject/scripts/main.js
@@ -10,14 +10,7 @@ window.addEventListener('load', function() {
 
 document.getElementById('add').addEventListener('click', addUsers);
 
-function sleep(milliseconds) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-      if ((new Date().getTime() - start) > milliseconds){
-        break;
-      }
-    }
-  }
+
 
 /*
 This Function is used to grab a single summoner information using the api call. In order to keep the data
